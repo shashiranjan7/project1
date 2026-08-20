b27d3540c985ad88de7bceddf204ee3b7fa31929
Shashi Raj <shashiranjan5829@gmail.com>
2026-08-20 20:17:47 +0530

    add

---
+++ practisethird.py
@@ -0,0 +1,28 @@
+movies=[]
+m1=input("enterm1:")
+m2=input("enterm2")
+m3=input("enterm3")
+# list=(m1,m2,m3)
+# print(list)or
+movies.append(m1)
+movies.append(m2)
+movies.append(m3)
+print(movies)
+list=[1,2,1]
+listcopy=list.copy()
+listcopy.reverse()
+if(list==listcopy):
+    print("is palindrome")
+else:
+    print("not palindrome")
+grade=["c","d","a","a","b","b","a"]
+print(grade.count("a"))
+print(grade.count("b"))
+print(grade.count("c"))
+print(grade.count("d"))
+grade.sort()
+print(grade) 
+
+
+
+
