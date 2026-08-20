f0f227da71c7e228e38d9e75baa17a99faa7a5a5
Shashi Raj <shashiranjan5829@gmail.com>
2026-08-20 19:18:51 +0530

    add practise.py

---
+++ practise.py
@@ -0,0 +1,28 @@
+a=int(input("enter a"))
+b=int(input("enter b"))
+print(a>=b)
+x=input("enter name")
+print(len(x))
+d=None
+print(type(d))
+import math
+y= math.sqrt(56)
+print(type(y))
+print(math.pi)
+print(y)
+n=int(input("enter n :",))
+if(n%2==0):
+    print("number is even")
+else:
+    print("no is odd")
+n1=int(input("enter no1"))
+n2=int(input("enter no2"))
+n3=int(input("enter no3"))
+if(n1>n2 and n1>n3):
+    print("n1 is greatest")
+elif(n2>n3 and n2>n1):
+    print("n2 is greatest")
+elif(n3>n1 and n3>n2):
+    print("n3 is greatest")
+
+       
